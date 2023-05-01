e8e38885e902f2c9921b05048bb8cb85a937f8b8
Michelle Fernandez <fernandezm525@outlook.es>
2023-05-01 13:10:52 -0500

    Modificaciones en el detalle del cliente

--- KingCourier/urls.py
+++ KingCourier/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("login.urls")),
-    path('clientes/', include("GestionClientes.urls")),
+    path('', include("GestionClientes.urls")),
     path('welcome/', TemplateView.as_view(template_name='welcome.html'), name='welcome'),
     path('mensajeros/', include("GestionMensajeros.urls")),
 ]
